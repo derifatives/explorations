a00ca1c62cdb1625c982c2652e8edb922c8e1640
Rif A. Saurous <rif@google.com>
2020-12-19 18:07:55 -0800

    Part two works too.

--- advent_of_code/2020/day_19.hs
+++ advent_of_code/2020/day_19.hs
@@ -1,26 +1,31 @@
 import Data.List
 import Data.List.Split
+import Data.List.Unique
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import System.Environment
 
--- Observations:
+-- Design concerns:
 --
--- 1. Every rule always expands into the same number of subrules, so
---    every full-parsed string will have the same length.
-
+-- We'll use [] to indicate a failure to parse. This is different from
+-- [[]], which indicates we parsed the entire input.
 main = do
   args <- getArgs
   content <- readFile (args !! 0)
   let [rulestrings, teststrings] = splitOn [""] (lines content)
       rules@(chr, ir) = parseRules rulestrings
-      accepted_teststrings = filter (\t -> goodParse (parseString rules t 0)) teststrings
+      parses =  (map (\t -> parseString rules t 0) teststrings)
 
-  putStrLn $ "Parse One, # accepted strings = " ++ (show (length accepted_teststrings))
-             
-goodParse  :: (String, Bool) -> Bool
-goodParse parse@(rest, status) = rest == "" && status == True
-             
+  -- inputStrLn $ (show (accepted_teststrings))
+  -- putStrLn $ (show (zip teststrings parses))
+  putStrLn $ (show (length (filter goodParse parses)))
+  --putStrLn $ (show (parseString rules "a" 4))
+  -- putStrLn $ (show (parseString rules "aba" 2))
+  -- putStrLn $ (show (parseString rules "ba" 4))
+
+goodParse :: [String] -> Bool
+goodParse = elem ""
+
 parseRules :: [String] -> (M.Map Int Char, M.Map Int [[Int]])
 parseRules = foldr parseOneRule (M.empty, M.empty)
 
@@ -38,17 +43,21 @@ parseIntLists words =
   let sublists = splitOn ["|"] words
   in map (map read) sublists
 
-parseString :: (M.Map Int Char, M.Map Int [[Int]]) -> String -> Int -> (String, Bool)
-parseString _ "" _ = ("", False)
+parseString :: (M.Map Int Char, M.Map Int [[Int]]) -> String -> Int -> [String]
+parseString _ "" _ = []
 parseString ruleset@(char_rules, int_rules) input@(h:rest) rule_id =
   if M.member rule_id char_rules
   then let jc@(Just c) = M.lookup rule_id char_rules
-       in if c == h then (rest, True) else (input, False)
+       in if c == h then [rest] else []
   else let jrl@(Just rule_lists) = M.lookup rule_id int_rules
-           parses = map (parseStringWithIntRules ruleset input) rule_lists
-           good_parse = find (\p@(_, good) -> good) parses
-       in if isJust good_parse then fromJust good_parse else (input, False)
+       in concat $ map (parseStringWithIntRules ruleset input) rule_lists
 
-parseStringWithIntRules :: (M.Map Int Char, M.Map Int [[Int]]) -> String -> [Int] -> (String, Bool)
-parseStringWithIntRules ruleset input intrules =
-  foldl' (\pp@(rest, good) rule -> if not good then pp else parseString ruleset rest rule) (input, True) intrules
+parseStringWithIntRules :: (M.Map Int Char, M.Map Int [[Int]]) -> String -> [Int] -> [String]
+parseStringWithIntRules ruleset input [] = [input]
+parseStringWithIntRules ruleset "" _ = []
+parseStringWithIntRules ruleset input (rule:rest) =
+  let rule_parses = unique $ parseString ruleset input rule
+      in if null rule_parses
+         then []
+         else concat $ map (\r -> unique $ parseStringWithIntRules ruleset r rest) rule_parses
+              
